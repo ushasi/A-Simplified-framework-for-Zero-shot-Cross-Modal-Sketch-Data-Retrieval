d2e67237a8d8200812d956382dcf5dd5f1602309
Ushasi Chaudhuri <ushasi2cool@gmail.com>
2020-07-28 14:27:44 +0200

    Update KNNcode.m

--- KNNcode.m
+++ KNNcode.m
@@ -1,24 +1,23 @@
 %new_feats = normc(fc_features)
 featsx = photo_feat_un;
 featsy = sketch_un;
-y = randsample(14591 , 50 );
-%y = [1801:1850];
+y = randsample(14591 , 400 ); #random query samples
 samples = y';
 q =size(samples) 
-t=64;
+t=64; #Dimension of the embedding space feature vectors
 
 clear distance; clear unified_knn;clear unified_Ind
 for i=1: q(2)       
     query = samples(i);
     
 
-    for x=1:2499%14591
+    for x=1:2499%14591  #unseen 25 class samples for Sketchy dataset
         sum=0;sum2=0;
         for y=1:t
             
-            sum2 = sum2 + sqrt((featsx(x,y) -  featsy(query,y)) * (featsx(x,y) -  featsy(query,y)));
-            %sum2 = sum2 + (abs(new_feats(x,y) -  new_feats(query,y)) );
-            %sum2 = sum2 + mahal(new_feats(x,:),new_feats(query,:))         
+            sum2 = sum2 + sqrt((featsx(x,y) -  featsy(query,y)) * (featsx(x,y) -  featsy(query,y))); # k-NN distance using l2 distance
+            %sum2 = sum2 + (abs(new_feats(x,y) -  new_feats(query,y)) ); # k-NN distance using l1 distance
+            %sum2 = sum2 + mahal(new_feats(x,:),new_feats(query,:))   # k-NN distance using Mahalanobis distance       
         end
         
         %sum2 = sqrt(sum2);
@@ -29,7 +28,8 @@ end
 [unified_knn, unified_Ind] = sort(distance, 1); 
 
 Ind = unified_Ind;
-%act = labels';
+
+%------------------------------ Actual label and Predicted labels-------------------------------------
 act = zeros(100,q(2));
 pred = zeros(100,q(2));
 for i=1:q(2)
@@ -40,10 +40,7 @@ for i=1:q(2)
     i;
 end
 
-display('tantanaaan');
-%pred = pred(1:100,:);
-%pred=pred';
-%act=act';
+%------------------------------------ Precision---------------------------------------------------------
 
 %% precision
 for n=1:10
@@ -63,9 +60,9 @@ for n=1:10
     n  
 end
 
+%-----------------------------------------Recall----------------------------------------------------
 rec= zeros(1,q(2));
 %% recall
-
 
 for n=1:10
     tp = 0; total = 100; reca = 0;
@@ -83,6 +80,7 @@ for n=1:10
     n
 end
 
+%------------------------------------ mean Average Precision---------------------------------------------------------
 %% MAP
 map = mean(precision);
 
